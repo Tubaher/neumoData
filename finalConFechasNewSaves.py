ad74e2b2fc28afd429ff90f515d96c7dc813ffc0
xxotto <otto092557009@hotmail.com>
2020-01-09 22:42:54 -0500

    agregados nuevos nombres para guardar

--- finalConFechasNewSaves.py
+++ finalConFechasNewSaves.py
@@ -4,26 +4,30 @@ import shortcuts
 
 inputs.time.sleep(15)
 historiasClinicas = []
-nPant=1
-verFechas.cargarHC(historiasClinicas, "pacfecha.csv")
+#nPant=1
+verFechas.cargarHC(historiasClinicas, "pac.txt")
 
 
 for hClinic in historiasClinicas:
+    nPant=1
     shortcuts.entrarHC(hClinic)
     stopExt = True
     ptCounter = 1
     csvFechas = verFechas.extractCSV("pacfecha.csv", hClinic)
     while stopExt:
         screenCp = shortcuts.tomarCaptura(hClinic + str(ptCounter))
-        ptCounter += 1
+        
         screenFechas = verFechas.extractFechasScreen(screenCp)
         
 
         
         for date in csvFechas:
+            #nFechRep = 1
             for i in range(0, len(screenFechas)):
+
                 if screenFechas[i] == date:
-                    nPant = shortcuts.recolectarHC(i, hClinic, date, nPant)
+                    nPant = shortcuts.recolectarHC(i, hClinic, date, nPant) #(ptCounter, nFechRep)
+                    #nFechRep + = 1
         
         for date in screenFechas:
             if int(date[0:4]) <= 2017:
@@ -34,7 +38,8 @@ for hClinic in historiasClinicas:
             inputs.pulsarTecla(inputs.Key.page_down)
         else:
             stopExt = False
-    
+        ptCounter += 1
+    inputs.combTeclas(inputs.Key.f3,inputs.Key.f3)
     inputs.pulsarTecla(inputs.Key.f3)
     inputs.pulsarTecla(inputs.Key.f3)
     inputs.time.sleep(1)
